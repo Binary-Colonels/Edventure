aacd32136378e769d2385d04cddd8a42a79f6d7e
Khusham Bansal <kbkhushambansal@gmail.com>
2025-04-27 02:45:19 +0530

    Add Next.js and Tailwind CSS configuration

---
+++ Edventure-frontend/next.config.js
@@ -0,0 +1,19 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  /* config options here */
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    domains: ['images.unsplash.com'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+};
+
+module.exports = nextConfig; 
